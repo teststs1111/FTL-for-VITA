bba3bf5e19901affd3bc914a35e7c80972c24ffa
teststs1111 <koh.shiki.24@gmail.com>
2026-09-23 00:57:16 +0900

    Render: extend texture handle

--- include/render/texture.hpp
+++ include/render/texture.hpp
@@ -1,21 +1,11 @@
 #pragma once
 #include <cstdint>
-
 namespace wormhole {
-
 class Texture {
 public:
-    Texture() = default;
-    Texture(int width, int height, std::uint32_t handle);
-    int width() const { return width_; }
-    int height() const { return height_; }
-    std::uint32_t handle() const { return handle_; }
-    bool valid() const { return handle_ != 0; }
-
-private:
-    int width_{0};
-    int height_{0};
-    std::uint32_t handle_{0};
+ Texture()=default; Texture(int width,int height,std::uint32_t handle);
+ int width() const{return width_;} int height() const{return height_;}
+ std::uint32_t handle() const{return handle_;} bool valid() const{return handle_!=0;}
+private: int width_{0}; int height_{0}; std::uint32_t handle_{0};
 };
-
 }
