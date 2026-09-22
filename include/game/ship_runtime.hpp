e448029d573ac2b1c54686ab062c3b6f6dd0f5ca
teststs1111 <koh.shiki.24@gmail.com>
2026-09-23 00:41:48 +0900

    Game: add room damage and repair operations

--- include/game/ship_runtime.hpp
+++ include/game/ship_runtime.hpp
@@ -1,16 +1,15 @@
 #pragma once
 #include "data/ship_content.hpp"
 #include <vector>
-
 namespace wormhole {
 struct ShipRuntime {
     LoadedShip content;
-    int hull{0};
-    int maxHull{0};
-    int reactor{0};
+    int hull{0}, maxHull{0}, reactor{0};
     std::vector<int> roomDamage;
     bool valid{false};
     bool load(ShipContent& source);
     void reset();
+    bool damageRoom(int roomId, int amount);
+    bool repairRoom(int roomId, int amount);
 };
 }
