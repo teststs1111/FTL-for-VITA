eb19d31559846a295c4ce8db5bef9057bd2905b3
teststs1111 <koh.shiki.24@gmail.com>
2026-09-23 00:56:48 +0900

    Render: add texture upload and draw API

--- src/render/graphics.cpp
+++ src/render/graphics.cpp
@@ -1,5 +1,7 @@
 #include "render/graphics.hpp"
 
+#include <vector>
+
 #ifdef __vita__
 #include <vitaGL.h>
 #include <psp2/gxm.h>
@@ -19,9 +21,6 @@ bool Graphics::init() {
 }
 
 void Graphics::shutdown() {
-#ifdef __vita__
-    // vitaGL does not expose a shutdown routine; resources are released on process exit.
-#endif
     initialized_ = false;
 }
 
@@ -30,6 +29,8 @@ void Graphics::beginFrame(const Color& clear) {
 #ifdef __vita__
     glClearColor(clear.r, clear.g, clear.b, clear.a);
     glClear(GL_COLOR_BUFFER_BIT);
+#else
+    (void)clear;
 #endif
 }
 
@@ -45,10 +46,8 @@ void Graphics::fillRect(float x, float y, float w, float h, const Color& color) 
 #ifdef __vita__
     glColor4f(color.r, color.g, color.b, color.a);
     glBegin(GL_QUADS);
-    glVertex2f(x, y);
-    glVertex2f(x + w, y);
-    glVertex2f(x + w, y + h);
-    glVertex2f(x, y + h);
+    glVertex2f(x, y); glVertex2f(x + w, y);
+    glVertex2f(x + w, y + h); glVertex2f(x, y + h);
     glEnd();
 #else
     (void)x; (void)y; (void)w; (void)h; (void)color;
@@ -60,11 +59,62 @@ void Graphics::drawLine(float x1, float y1, float x2, float y2, const Color& col
 #ifdef __vita__
     glColor4f(color.r, color.g, color.b, color.a);
     glBegin(GL_LINES);
-    glVertex2f(x1, y1);
-    glVertex2f(x2, y2);
+    glVertex2f(x1, y1); glVertex2f(x2, y2);
     glEnd();
 #else
     (void)x1; (void)y1; (void)x2; (void)y2; (void)color;
+#endif
+}
+
+Texture Graphics::createTexture(const std::vector<std::uint8_t>& rgba, int width, int height) {
+    if (!initialized_ || width <= 0 || height <= 0 ||
+        rgba.size() != static_cast<std::size_t>(width) * height * 4)
+        return {};
+
+#ifdef __vita__
+    GLuint handle = 0;
+    glGenTextures(1, &handle);
+    if (!handle) return {};
+    glBindTexture(GL_TEXTURE_2D, handle);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, rgba.data());
+    glBindTexture(GL_TEXTURE_2D, 0);
+    return Texture(width, height, handle);
+#else
+    (void)rgba; (void)width; (void)height;
+    return {};
+#endif
+}
+
+void Graphics::destroyTexture(Texture& texture) {
+#ifdef __vita__
+    if (texture.handle()) {
+        const GLuint handle = static_cast<GLuint>(texture.handle());
+        glDeleteTextures(1, &handle);
+    }
+#endif
+    texture = {};
+}
+
+void Graphics::drawTexture(const Texture& texture, float x, float y, float w, float h, const Color& color) {
+    if (!initialized_ || !texture.valid()) return;
+#ifdef __vita__
+    glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D, static_cast<GLuint>(texture.handle()));
+    glColor4f(color.r, color.g, color.b, color.a);
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.f, 0.f); glVertex2f(x, y);
+    glTexCoord2f(1.f, 0.f); glVertex2f(x + w, y);
+    glTexCoord2f(1.f, 1.f); glVertex2f(x + w, y + h);
+    glTexCoord2f(0.f, 1.f); glVertex2f(x, y + h);
+    glEnd();
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glDisable(GL_TEXTURE_2D);
+#else
+    (void)texture; (void)x; (void)y; (void)w; (void)h; (void)color;
 #endif
 }
 
