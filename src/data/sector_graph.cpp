e77e0c7a1178d4098353d79807937bee8df8f06b
teststs1111 <koh.shiki.24@gmail.com>
2026-09-26 00:38:36 +0900

    Fix sector graph build errors

--- src/data/sector_graph.cpp
+++ src/data/sector_graph.cpp
@@ -30,7 +30,7 @@ void SectorGraph::generate(int sector, std::uint32_t seed) {
             }
         }
     }
-    startNode_=static_cast<int>(rng()%columns);
+    startNode_=static_cast<int>(rng()%columns_);
     nodes_[startNode_].visited=true;
 }
 
